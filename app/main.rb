00f565c8ec799f0428d4b29692a7dd2df8832f2c
Cao Van Bi <c-bi@ruby-dev.jp>
2022-07-30 22:23:05 +0900

    Read a blob object

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+require 'zlib'
 # You can use print statements as follows for debugging, they'll be visible when running tests.
 puts "Logs from your program will appear here!"
 
@@ -11,6 +13,11 @@ when "init"
   Dir.mkdir(".git/refs")
   File.write(".git/HEAD", "ref: refs/heads/master\n")
   puts "Initialized git directory"
+when "cat-file"
+  object_hash = ARGV[2]
+  path = ".git/objects/#{object_hash[0,2]}/#{object_hash[2,38]}"
+  cstr = File.read(path)
+  puts Zlib::Inflate.inflate(cstr)
 else
   raise RuntimeError.new("Unknown command #{command}")
 end
