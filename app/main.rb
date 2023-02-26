da2742cbab648dd45aedb6b434aa808afafb05bc
Cao Van Bi <c-bi@ruby-dev.jp>
2023-02-27 05:45:21 +0900

    Refactor

--- app/main.rb
+++ app/main.rb
@@ -3,8 +3,6 @@ require 'zlib'
 # You can use print statements as follows for debugging, they'll be visible when running tests.
 # puts "Logs from your program will appear here!"
 
-# Uncomment this block to pass the first stage
-#
 command = ARGV[0]
 case command
 when "init"
@@ -15,21 +13,18 @@ when "init"
   puts "Initialized git directory"
 when "cat-file"
   object_hash = ARGV[2]
-  path = ".git/objects/#{object_hash[0,2]}/#{object_hash[2,38]}"
-  cstr = File.read(path)
-  store = Zlib::Inflate.inflate(cstr)
+  path = ".git/objects/#{object_hash[...2]}/#{object_hash[2..]}"
+  store = Zlib::Inflate.inflate(File.read(path))
   header, content = store.split("\0")
   print content.strip
 when "hash-object"
-  file = ARGV[2]
-  content = File.read(file)
-  header = "blob #{content.length}\0"
-  store = header + content
+  content = File.read(ARGV[2])
+  store = "blob #{content.length}\0#{content}"
   sha1 = Digest::SHA1.hexdigest(store)
-  zlib_content = Zlib::Deflate.deflate(store)
-  path = ".git/objects/#{sha1[0,2]}/#{sha1[2,38]}"
+  path = ".git/objects/#{sha1[...2]}/#{sha1[2..]}"
   Dir.mkdir(File.dirname(path))
-  File.open(path, "w") { |f| f.write(zlib_content) }
+  compress_content = Zlib::Deflate.deflate(store)
+  File.open(path, "w") { |f| f.write(compress_content) }
   print sha1
 else
   raise RuntimeError.new("Unknown command #{command}")
